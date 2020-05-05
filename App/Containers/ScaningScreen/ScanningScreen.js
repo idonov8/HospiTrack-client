893f43845a9edeafea54367fbf1fbad8618b1ab9
Ido Nov <idonov8@gmail.com>
2020-05-05 07:43:15 +0300

    remove unneeded screens and add single screen stub

---
+++ App/Containers/ScaningScreen/ScanningScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default class SplashScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <View>
+            <Text>Scan stub</Text>
+        </View>
+      </View>
+    )
+  }
+}
